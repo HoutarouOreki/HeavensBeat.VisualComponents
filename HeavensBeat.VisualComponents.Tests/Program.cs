ff28bfc274fefb7f55fe1220906e569b7ba41b63
Hōtarō Oreki <TheZjarek@gmail.com>
2020-04-29 18:34:39 +0200

    Remove unused argument

--- HeavensBeat.VisualComponents.Tests/Program.cs
+++ HeavensBeat.VisualComponents.Tests/Program.cs
@@ -4,7 +4,7 @@ namespace HeavensBeat.VisualComponents.Tests
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             using var host = Host.GetSuitableHost("HeavensBeat.VisualComponents.Tests");
             host.Run(new TestBrowserContainer());
